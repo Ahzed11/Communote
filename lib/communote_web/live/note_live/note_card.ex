2280d4ba50bdec48fc0e355cc5517162d2c8b78e
Ahzed11 <ahzed11@protonmail.com>
2022-07-13 22:08:12 +0200

    Removed an unwanted character

--- lib/communote_web/live/note_live/note_card.ex
+++ lib/communote_web/live/note_live/note_card.ex
@@ -8,7 +8,7 @@ defmodule CommunoteWeb.Live.NoteLive.NoteCard do
         <%= live_redirect to: Routes.note_show_path(@socket, :show, note.slug), class: "h-full" do %>
             <div class="p-8 flex flex-col justify-between h-full">
                 <div>
-                    <h2 class="text-2xl font-bold truncate"><%= note.title %>z</h2>
+                    <h2 class="text-2xl font-bold truncate"><%= note.title %></h2>
                     <div class="text-purple-800 dark:text-green-600 text-sm font-medium flex mb-4">
                         <span>
                             <%= note.user.first_name %> <%= note.user.last_name %>
