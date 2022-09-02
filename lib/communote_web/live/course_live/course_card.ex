c82e09d40de3539715d30f17fe8918ade6c3a889
Ahzed11 <ahzed11@protonmail.com>
2022-09-02 10:55:20 +0200

    Improved hitbox on course card

--- lib/communote_web/live/course_live/course_card.ex
+++ lib/communote_web/live/course_live/course_card.ex
@@ -3,12 +3,11 @@ defmodule CommunoteWeb.Live.CourseLive.CourseCard do
 
     def render(assigns) do
       ~L"""
-      <li class="p-8 inline-block rounded-lg shadow overflow-hidden h-full hover:shadow-lg transform hover:scale-105 transition duration-300
-      bg-white dark:bg-gray-800 dark:text-white"
+      <li class=""
         id={"course-#{course.id}"}
         >
-            <div class="h-full">
-            <%= live_redirect to: Routes.note_index_path(@socket, :index, assigns.course.code) do %>
+            <%= live_redirect to: Routes.note_index_path(@socket, :index, assigns.course.code), class: "p-8 inline-block rounded-lg shadow overflow-hidden h-full hover:shadow-lg transform hover:scale-105 transition duration-300
+            bg-white dark:bg-gray-800 dark:text-white" do %>
                 <div class="h-full">
                     <h2 class="text-2xl font-bold"><%= assigns.course.title %></h2>
                     <div class="flex justify-between text-sm font-semibold mt-2 text-purple-800 dark:text-green-600">
@@ -19,7 +18,6 @@ defmodule CommunoteWeb.Live.CourseLive.CourseCard do
                     </div>
                 </div>
             <% end %>
-            </div>
         </li>
       """
     end
